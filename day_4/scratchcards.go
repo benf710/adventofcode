2ae1b67bf5389e2072e1406ccb60d5d8d46a7f09
Ben Foltz <ben.h.foltz@gmail.com>
2023-12-06 19:29:29 -0500

    changes for day 4 part 1

--- day_4/scratchcards.go
+++ day_4/scratchcards.go
@@ -8,11 +8,21 @@ import (
 	"github.com/benf710/adventofcode/common"
 )
 
+func removeSpaces(s []string) []string {
+	var ret []string
+	for _, str := range s {
+		if strings.TrimSpace(str) != "" {
+			ret = append(ret, str)
+		}
+	}
+	return ret
+}
+
 func parseLine(line string) ([]string, []string) {
 	_, nums, _ := strings.Cut(line, ":")
 	allNums := strings.Split(nums, "|")
-	winnersStr := strings.Split(allNums[0], " ")
-	cardNumsStr := strings.Split(allNums[1], " ")
+	winnersStr := removeSpaces(strings.Split(allNums[0], " "))
+	cardNumsStr := removeSpaces(strings.Split(allNums[1], " "))
 	return winnersStr, cardNumsStr
 }
 
@@ -25,10 +35,12 @@ func main() {
 		}
 		winners, cardNums := parseLine(line)
 		winningCardNums := 0
+		var matchedNums []string
 		for _, cardNum := range cardNums {
 			for _, winner := range winners {
 				if cardNum == winner {
 					winningCardNums++
+					matchedNums = append(matchedNums, cardNum)
 					break
 				}
 			}
@@ -40,10 +52,10 @@ func main() {
 		} else if winningCardNums == 1 {
 			cardPoints = 1
 		} else {
-			cardPoints = int(math.Pow(2, float64(winningCardNums)))
+			cardPoints = int(math.Pow(2, float64(winningCardNums)-1))
 		}
 		points += cardPoints
-		fmt.Printf("Card points: %d\n", cardPoints)
+		fmt.Printf("Matched nums: %q\nCard points: %d\n", matchedNums, cardPoints)
 	}
-	fmt.Printf("Total points: %d\n", points)
+	fmt.Printf("Total points: %d\n", points) // 646754 too high 323378 too high
 }
